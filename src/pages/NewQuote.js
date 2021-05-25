6804ccf538e00069c5f7b6cb237876e0181f9b38
Raul Mercado <raul.mercado.garcia@ericsson.com>
2021-05-25 18:29:33 -0500

    Se agrega formularios de lista y nuevo quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,10 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-    return <h1>New Quote</h1>
+    const addQuoteHandler = (quote) => {
+        console.log(quote);
+    }
+    return <QuoteForm onAddQuote={addQuoteHandler()} />
 }
 
 export default NewQuote;
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    {id: 1, author: "Raul Mercado", text:"Todo se puede con esfuerzo"},
+    {id: 2, author: "Juan Perez", text:"A vivir la vida loca"},
+]
+
 const Quotes = () => {
-    return <h1>Quotes</h1>
+    return <QuoteList quotes={DUMMY_QUOTES}/>
 }
 
 export default Quotes;

34c72fbb4904cc66adc1f64433bccfac0e74ef0b
Raul Mercado <raulmercadox@gmail.com>
2021-05-26 00:31:20 -0500

    Se agrega formularios de quotes

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,11 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import {useHistory} from 'react-router-dom';
 
 const NewQuote = () => {
+    const history = useHistory();
     const addQuoteHandler = (quote) => {
         console.log(quote);
+        history.push('/quotes');
     }
     return <QuoteForm onAddQuote={addQuoteHandler} />
 }
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,23 @@
 import {useParams, Route} from 'react-router-dom'
 import React, {Fragment} from 'react';
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    {id: 1, author: "Raul Mercado", text:"Todo se puede con esfuerzo"},
+    {id: 2, author: "Juan Perez", text:"A vivir la vida loca"},
+]
 
 const QuoteDetail = () => {
     const params = useParams();
+    const quote = DUMMY_QUOTES.find(q => q.id === Number(params.quoteId));
+    if (!quote)
+    {
+        return <div>Quote not found</div>
+    }
 
     return <Fragment>
-        <h1>Quote Detail {params.quoteId}</h1>
+        <HighlightedQuote text={quote.text} author={quote.author}/>
         <Route path="/quotes/:quoteId/comments">
             <Comments/>
         </Route>
